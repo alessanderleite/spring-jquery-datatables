317919bf25e67ffb7e4035d18a0a625224ed0861
alessanderleite <alessander.logistica@gmail.com>
2019-07-20 17:57:31 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/app/SpringJQueryDataTablesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJQueryDataTablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJQueryDataTablesApplication.class, args);
+	}
+
+}
