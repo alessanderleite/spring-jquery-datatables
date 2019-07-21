904be84bbb61ebf33e0886570bff28e517953885
alessanderleite <alessander.logistica@gmail.com>
2019-07-20 22:44:19 -0300

    refactoring the EmployeeController.java

--- src/main/java/br/com/alessanderleite/app/controller/EmployeeController.java
+++ src/main/java/br/com/alessanderleite/app/controller/EmployeeController.java
@@ -1,4 +1,4 @@
-package br.com.alessanderleite.app;
+package br.com.alessanderleite.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
