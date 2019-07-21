c55d3e91d67f605d58dc5251a35e0b9711ef732a
alessanderleite <alessander.logistica@gmail.com>
2019-07-20 22:33:36 -0300

    call Optional<>

--- src/main/java/br/com/alessanderleite/app/service/EmployeeService.java
+++ src/main/java/br/com/alessanderleite/app/service/EmployeeService.java
@@ -1,11 +1,12 @@
 package br.com.alessanderleite.app.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import br.com.alessanderleite.app.model.Employee;
 
 public interface EmployeeService {
 	
 	List<Employee> getAllEmployees();
-	Employee getEmployeeById(long id);
+	Optional<Employee> getEmployeeById(long id);
 }
